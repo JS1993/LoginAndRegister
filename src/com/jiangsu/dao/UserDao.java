69788713d4f2d089275bdcd7fe1b3a4d97a7e599
jiangsu <28025920@qq.com>
2017-03-16 15:59:25 +0800

    完成项目

---
+++ src/com/jiangsu/dao/UserDao.java
@@ -0,0 +1,13 @@
+package com.jiangsu.dao;
+
+import com.jiangsu.domain.User;
+
+public interface UserDao {
+	
+	/**
+	 * ÃÌº””√ªß
+	 * @param user
+	 * @throws Exception
+	 */
+	public void addUser(User user) throws Exception;
+}
---
+++ src/com/jiangsu/dao/impl/UserDaoImpl.java
@@ -0,0 +1,16 @@
+package com.jiangsu.dao.impl;
+
+import com.jiangsu.dao.UserDao;
+import com.jiangsu.domain.User;
+
+public class UserDaoImpl implements UserDao{
+
+	@Override
+	public void addUser(User user) throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
+
+	
+
+}
